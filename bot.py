635a05ca9d9df5143ec3e2fa83eac14f7e19ef97
laylysik <58001891+laylysik@users.noreply.github.com>
2019-11-20 21:49:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,67 +1,81 @@
-# -*- coding: utf-8 -*-
-
 import telebot
-import config
-import dbworker
+import bs4
+from Task import Task
+import parser
+import markups as m
 
-bot = telebot.TeleBot(config.token)
+#main variables
+TOKEN = '1029407549:AAHNiAJkrkGII6igOHTJYdpKbSGTNnVrJQ8'
+bot = telebot.TeleBot(TOKEN)
+task = Task()
 
+#handlers
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    if not task.isRunning:
+        chat_id = message.chat.id
+        msg = bot.send_message(chat_id, 'Откуда парсить?', reply_markup=m.source_markup)
+        bot.register_next_step_handler(msg, askSource)
+        task.isRunning = True
 
-# Начало диалога
-@bot.message_handler(commands=["start"])
-def cmd_start(message):
-    state = dbworker.get_current_state(message.chat.id)
-    if state == config.States.S_ENTER_NAME.value:
-        bot.send_message(message.chat.id, "Кажется, кто-то обещал отправить своё имя, но так и не сделал этого :( Жду...")
-    elif state == config.States.S_ENTER_AGE.value:
-        bot.send_message(message.chat.id, "Кажется, кто-то обещал отправить свой возраст, но так и не сделал этого :( Жду...")
-    elif state == config.States.S_SEND_PIC.value:
-        bot.send_message(message.chat.id, "Кажется, кто-то обещал отправить картинку, но так и не сделал этого :( Жду...")
-    else:  # Под "остальным" понимаем состояние "0" - начало диалога
-        bot.send_message(message.chat.id, "Привет! Как я могу к тебе обращаться?")
-        dbworker.set_state(message.chat.id, config.States.S_ENTER_NAME.value)
-
-
-# По команде /reset будем сбрасывать состояния, возвращаясь к началу диалога
-@bot.message_handler(commands=["reset"])
-def cmd_reset(message):
-    bot.send_message(message.chat.id, "Что ж, начнём по-новой. Как тебя зовут?")
-    dbworker.set_state(message.chat.id, config.States.S_ENTER_NAME.value)
-
-
-@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_ENTER_NAME.value)
-def user_entering_name(message):
-    # В случае с именем не будем ничего проверять, пусть хоть "25671", хоть Евкакий
-    bot.send_message(message.chat.id, "Отличное имя, запомню! Теперь укажи, пожалуйста, свой возраст.")
-    dbworker.set_state(message.chat.id, config.States.S_ENTER_AGE.value)
-
-
-@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_ENTER_AGE.value)
-def user_entering_age(message):
-    # А вот тут сделаем проверку
-    if not message.text.isdigit():
-        # Состояние не меняем, поэтому только выводим сообщение об ошибке и ждём дальше
-        bot.send_message(message.chat.id, "Что-то не так, попробуй ещё раз!")
-        return
-    # На данном этапе мы уверены, что message.text можно преобразовать в число, поэтому ничем не рискуем
-    if int(message.text) < 5 or int(message.text) > 100:
-        bot.send_message(message.chat.id, "Какой-то странный возраст. Не верю! Отвечай честно.")
-        return
+def askSource(message):
+    chat_id = message.chat.id
+    text = message.text.lower()
+    if text in task.names[0]:
+        task.mySource = 'top'
+        msg = bot.send_message(chat_id, 'За какой временной промежуток?', reply_markup=m.age_markup)
+        bot.register_next_step_handler(msg, askAge)
+    elif text in task.names[1]:
+        task.mySource = 'all'
+        msg = bot.send_message(chat_id, 'Какой минимальный порог рейтинга?', reply_markup=m.rating_markup)
+        bot.register_next_step_handler(msg, askRating)
     else:
-        # Возраст введён корректно, можно идти дальше
-        bot.send_message(message.chat.id, "Когда-то и мне было столько лет...эх... Впрочем, не будем отвлекаться. "
-                                          "Отправь мне какую-нибудь фотографию.")
-        dbworker.set_state(message.chat.id, config.States.S_SEND_PIC.value)
+        msg = bot.send_message(chat_id, 'Такого раздела нет. Введите раздел корректно.')
+        bot.register_next_step_handler(msg, askSource)
+        return
 
+def askAge(message):
+    chat_id = message.chat.id
+    text = message.text.lower()
+    filters = task.filters[0]
+    if text not in filters:
+        msg = bot.send_message(chat_id, 'Такого временного промежутка нет. Введите порог корректно.')
+        bot.register_next_step_handler(msg, askAge)
+        return
+    task.myFilter = task.filters_code_names[0][filters.index(text)]
+    msg = bot.send_message(chat_id, 'Сколько страниц парсить?', reply_markup=m.amount_markup)
+    bot.register_next_step_handler(msg, askAmount)
 
-@bot.message_handler(content_types=["photo"],
-                     func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_SEND_PIC.value)
-def user_sending_photo(message):
-    # То, что это фотография, мы уже проверили в хэндлере, никаких дополнительных действий не нужно.
-    bot.send_message(message.chat.id, "Отлично! Больше от тебя ничего не требуется. Если захочешь пообщаться снова - "
-                     "отправь команду /start.")
-    dbworker.set_state(message.chat.id, config.States.S_START.value)
+def askRating(message):
+    chat_id = message.chat.id
+    text = message.text.lower()
+    filters = task.filters[1]
+    if text not in filters:
+        msg = bot.send_message(chat_id, 'Такого порога нет. Введите порог корректно.')
+        bot.register_next_step_handler(msg, askRating)
+        return
+    task.myFilter = task.filters_code_names[1][filters.index(text)]
+    msg = bot.send_message(chat_id, 'Сколько страниц парсить?', reply_markup=m.amount_markup)
+    bot.register_next_step_handler(msg, askAmount)
 
+def askAmount(message):
+    chat_id = message.chat.id
+    text = message.text.lower()
+    if not text.isdigit():
+        msg = bot.send_message(chat_id, 'Количество страниц должно быть числом. Введите корректно.')
+        bot.register_next_step_handler(msg, askAmount)
+        return
+    if int(text) < 1 or int(text) > 5:
+        msg = bot.send_message(chat_id, 'Количество страниц должно быть >0 и <6. Введите корректно.')
+        bot.register_next_step_handler(msg, askAmount)
+        return
+    task.isRunning = False
+    print(task.mySource + " | " + task.myFilter + ' | ' + text) #
+    output = ''
+    if task.mySource == 'top':
+        output = parser.getTitlesFromTop(int(text), task.myFilter)
+    else:
+        output = parser.getTitlesFromAll(int(text), task.myFilter)
+    msg = bot.send_message(chat_id, output, reply_markup=m.start_markup)
 
-if __name__ == "__main__":
-    bot.polling(none_stop=True)
+bot.polling(none_stop=True)
